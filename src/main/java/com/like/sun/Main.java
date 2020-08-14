e6b4eaf3303bef243d57fdd3c4ef351252aba382
sun5066 <alstjr4434@naver.com>
2020-08-14 16:42:57 +0900

    08-14

--- src/main/java/com/like/sun/Main.java
+++ src/main/java/com/like/sun/Main.java
@@ -6,10 +6,6 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -26,16 +22,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
 
-//        System.out.println("검색할 동영상 제목입력 : ");
-        String stringJson = "";
+        String searchJson = "";
         try {
-            stringJson = search("이하이 홀로");
+            searchJson = search("이하이 홀로");
         } catch (IOException exception) {
             System.out.println("아마도 트래픽이 초과된듯 싶습니다.");
             exception.printStackTrace();
             return;
         }
 
+        List<YoutubeVO> youtubeList = getYouTubeData(searchJson);
+        if (!youtubeList(youtubeList)) {
+            System.out.println("데이터가 없습니다.");
+        }
+    }
+
+    private static List<YoutubeVO> getYouTubeData(String stringJson) {
         JSONParser jsonParser = new JSONParser();
         Object object = null;
         try {
@@ -43,9 +45,11 @@ public class Main {
 
             JSONObject jsonObject = (JSONObject) object;
             JSONArray jsonArray = (JSONArray) jsonObject.get("items");
-            JSONObject tempObject = null;
+
             List<YoutubeVO> youtubeList = new ArrayList<YoutubeVO>();
+            JSONObject tempObject = null;
             YoutubeVO youtubeVO = null;
+
             for (int i = 0; i < 3; i++) {
                 youtubeVO = new YoutubeVO();
                 tempObject = (JSONObject) jsonArray.get(i);
@@ -57,17 +61,41 @@ public class Main {
                 youtubeVO.setUrl(String.format("https://www.youtube.com/watch?v=%s", youtubeVO.getVideoId()));
                 youtubeList.add(youtubeVO);
             }
-
-            for (YoutubeVO ytVO : youtubeList) {
-                System.out.println(String.format("title\t: %s", ytVO.getTitle()));
-                System.out.println(String.format("url\t\t: %s", ytVO.getUrl()));
-                System.out.println(String.format("videoID\t: %s", ytVO.getVideoId()));
-                System.out.println(String.format("image\t: https://i.ytimg.com/vi/%s/maxresdefault.jpg", ytVO.getVideoId()));
-                System.out.println();
-            }
+            return youtubeList;
         } catch (ParseException e) {
             System.out.println("JSON 형변환 중 예외발생! JSON 형식이 아닌것같습니다.");
         }
+        return null;
+    }
+
+    private static void getDuration(String stringJson) {
+        JSONParser jsonParser = new JSONParser();
+        Object object = null;
+        try {
+            object = jsonParser.parse(stringJson);
+
+            JSONObject jsonObject = (JSONObject) object;
+            JSONArray jsonArray = (JSONArray) jsonObject.get("items");
+
+            System.out.println(jsonArray);
+        } catch (ParseException e) {
+            System.out.println("JSON 형변환 중 예외발생! JSON 형식이 아닌것같습니다.");
+        }
+    }
+
+    private static boolean youtubeList(List<YoutubeVO> youtubeList) throws IOException {
+        if (youtubeList == null) {
+            return false;
+        }
+        for (YoutubeVO ytVO : youtubeList) {
+            System.out.println(String.format("title\t: %s", ytVO.getTitle()));
+            System.out.println(String.format("url\t\t: %s", ytVO.getUrl()));
+            System.out.println(String.format("videoID\t: %s", ytVO.getVideoId()));
+            System.out.println(String.format("image\t: https://i.ytimg.com/vi/%s/maxresdefault.jpg", ytVO.getVideoId()));
+            System.out.println(video(ytVO.getVideoId()));
+            System.out.println();
+        }
+        return true;
     }
 
     private static String search(String search) throws IOException {
@@ -76,6 +104,19 @@ public class Main {
         apiurl += "&part=snippet&type=video&maxResults=20&videoEmbeddable=true";
         apiurl += "&q=" + URLEncoder.encode(search, "UTF-8");
 
+        return getString(apiurl);
+    }
+
+    private static String video(String id) throws IOException {
+        String apiurl = "https://www.googleapis.com/youtube/v3/videos";
+        apiurl += "?id=" + id;
+        apiurl += "&part=contentDetails";
+        apiurl += "&key=AIzaSyAGWOYDPwcoSSkC7ck2eLZNulFRHLx_VMo";
+
+        return getString(apiurl);
+    }
+
+    private static String getString(String apiurl) throws IOException {
         URL url = new URL(apiurl);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
@@ -89,8 +130,4 @@ public class Main {
         br.close();
         return response.toString();
     }
-
-//    private static String video(String url) {
-//
-//    }
 }
